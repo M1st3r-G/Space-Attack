e326cd06a134910543eeb6ddab6e4d55454ec5ac
MisterG <g.haegerbaeumer@gmx.de>
2023-12-01 10:22:56 +0100

    Added A GameManager Spawning Enemys
    Enemys now have simplpe Jittering

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //ComponentReferences
+    [SerializeField] private GameObject enemy;
+    //Params
+    [SerializeField] private Vector2Int grid;
+    [SerializeField] private Vector2 spawnPostion;
+    [SerializeField] private float padding;
+    //Temps
+    private List<EnemyController> allEnemies;
+    //Publics
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+     
+    private void Awake()
     {
+        if (_instance is not null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
         
+        allEnemies = new List<EnemyController>();
+        SpawnEnemies();
+        
+        DontDestroyOnLoad(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemies()
     {
-        
+        var offset = (Vector2)(grid - new Vector2Int(1,1)) * (1 + padding) / 2;
+        for (int i = 0; i < grid.x; i++)
+        {
+            for (int j = 0; j < grid.y; j++)
+            {
+                Vector2 pos = new Vector2(i*(1+padding), j*(1+padding)) - offset;
+                GameObject tmp = Instantiate(enemy, pos , Quaternion.identity);
+                var deb = tmp.GetComponent<EnemyController>();
+                print(deb.name);
+                allEnemies.Add(deb);
+            }
+        }
     }
 }
