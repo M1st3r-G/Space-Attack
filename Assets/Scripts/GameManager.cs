ff01d5b3055551c1b7469185cde356ba9ef90741
MisterG <g.haegerbaeumer@gmx.de>
2023-12-01 11:23:05 +0100

    Added Life and Points UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     //ComponentReferences
     [SerializeField] private GameObject enemy;
+    [SerializeField] private TextMeshProUGUI lifeText;
+    [SerializeField] private TextMeshProUGUI pointsText;
     //Params
     [SerializeField] private Vector2Int grid;
     [SerializeField] private Vector2 spawnPostion;
     [SerializeField] private float padding;
+    [SerializeField, Range(1,3)] private int typeToSpawn;
+    [SerializeField] private EnemyStats[] types;
+    
     //Temps
     private List<EnemyController> allEnemies;
+    private int points
+    {
+        set {pointsText.text = value.ToString(); }
+    }
+    private int life
+    {
+        set {lifeText.text = value.ToString(); }
+    }
+    
     //Publics
     private static GameManager _instance;
     public static GameManager Instance => _instance;
@@ -26,6 +43,7 @@ public class GameManager : MonoBehaviour
         _instance = this;
         
         allEnemies = new List<EnemyController>();
+        points = life = 0;
         SpawnEnemies();
         
         DontDestroyOnLoad(this);
@@ -41,6 +59,7 @@ public class GameManager : MonoBehaviour
                 Vector2 pos = new Vector2(i*(1+padding), j*(1+padding)) - offset;
                 GameObject tmp = Instantiate(enemy, pos , Quaternion.identity);
                 var deb = tmp.GetComponent<EnemyController>();
+                deb.setStats(types[typeToSpawn]);
                 print(deb.name);
                 allEnemies.Add(deb);
             }
